b674b708392370b4a6261024e16333eeed98c767
ebrahim-j <ebzjanoo@gmail.com>
2017-02-08 22:06:35 +0300

    Update file
    Made provisions for main function to execute temperature method. and added comments for clarity

--- TemperatureByCity.py
+++ TemperatureByCity.py
@@ -1,10 +1,22 @@
-def temperature(city):
+import requests
+
+def temperature():
+	#getting input from user
 	city = input("Enter City: ")
+	#making an API call to openweathermap
 	r = requests.get("http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=402c7b6d34f8b5e233a424147c2953bf")
-	json_obj = r.json()
-	temp_k = float(json_obj['main']['temp'] )
-	temp_c = temp_k - 273.15
-	return str(temp_c)
+	json_obj = r.json() #retrieving JSON data and saving to json_obj
+	#to check for unavailable data
+    #if json_obj == None:
+    #    print("Oops! We don't know this city!")
+    #else:
+	temp_k = float(json_obj['main']['temp']) #parsing json 
+	temp_c = temp_k - 273.15 #converting temperature to Celcius
+	print ("The current temperature is: "+ str(temp_c) + "C")
+
+
+def main():
+	temperature()
 
 if __name__ == '__main__':
 	main()
